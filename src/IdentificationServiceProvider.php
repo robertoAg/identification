1f698727efe8d26bed03130de9cfe01e77f7c15a
raguilar <raguilar@msolutions.es>
2018-06-08 09:27:39 +0200

    one more try

--- src/IdentificationServiceProvider.php
+++ src/IdentificationServiceProvider.php
@@ -24,10 +24,5 @@ class IdentificationServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this->app->singleton(IdentificationMiddleware::class, function () {
-            return new IdentificationMiddleware();
-        });
-
-        $this->app->alias(IdentificationMiddleware::class, 'identification-middleware');
     }
 }
